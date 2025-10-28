6fe3baf8ccd815407d8ee5b9b11fa4f838226c3a
Anastasia <3540455@bk.ru>
2025-10-28 22:48:48 +0300

    add the element with the task: checkbox, description, delete button

--- js/script.js
+++ js/script.js
@@ -20,6 +20,32 @@ function test() {
     button.addEventListener("click", () => {
         console.log("clicked the test button");
     })
+
+    const list = document.createElement("ul");
+    list.id = "listId"
+    document.body.append(list);
+
+    button.addEventListener("click", () => {
+        const textTask = input.value.trim();
+        if (!textTask) return;
+
+        const point = document.createElement("li");
+
+        const box_with_sign = document.createElement("input");
+        box_with_sign.type = "checkbox";
+
+        const taskFull = document.createElement("span");
+        taskFull.textContent = textTask;
+
+        const deleteButton = document.createElement("button");
+        deleteButton.type = "button";
+        deleteButton.textContent = 'Удалить'
+
+        point.append(box_with_sign, taskFull, deleteButton);
+        list.append(point);
+
+        input.value = ""
+    })
 }
 
 test();
