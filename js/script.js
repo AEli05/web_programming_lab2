e2a61bed2447108ac979046fbe215f73fdb1aab2
Anastasia <3540455@bk.ru>
2025-10-28 22:26:42 +0300

    add the elements of field, button and test it with console

--- js/script.js
+++ js/script.js
@@ -4,6 +4,22 @@ function test() {
     const title = document.createElement("h1");
     title.textContent = "My TO-DO LIST";
     document.body.append(title);
+
+    const container = document.createElement("div");
+    document.body.append(container);
+
+    const input = document.createElement("input");
+    input.type = "text";
+    input.placeholder = 'Введите Вашу задачу......'
+    document.body.append(input);
+
+    const button = document.createElement("button");
+    button.textContent = 'Добавить'
+    document.body.append(button);
+
+    button.addEventListener("click", () => {
+        console.log("clicked the test button");
+    })
 }
 
 test();
